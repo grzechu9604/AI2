ad15698af416f5fc7451bdaeaf9688ed5a6bf13e
Grzegorz Wilczyński <grzegorz.wilczynski@student.put.poznan.pl>
2018-06-13 12:42:29 +0200

    Automatycznie wygenerowane controllery

---
+++ AILibrary/Controllers/BookCopiesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AILibrary.Models;
+using AILibrary.Models.Books;
+
+namespace AILibrary.Controllers
+{
+    public class BookCopiesController : Controller
+    {
+        private LibraryDBContext db = new LibraryDBContext();
+
+        // GET: BookCopies
+        public ActionResult Index()
+        {
+            return View(db.BookCopies.ToList());
+        }
+
+        // GET: BookCopies/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookCopy bookCopy = db.BookCopies.Find(id);
+            if (bookCopy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookCopy);
+        }
+
+        // GET: BookCopies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookCopies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,PossesorId,CurrentlyPossesdByUserId")] BookCopy bookCopy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BookCopies.Add(bookCopy);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bookCopy);
+        }
+
+        // GET: BookCopies/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookCopy bookCopy = db.BookCopies.Find(id);
+            if (bookCopy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookCopy);
+        }
+
+        // POST: BookCopies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,PossesorId,CurrentlyPossesdByUserId")] BookCopy bookCopy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bookCopy).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bookCopy);
+        }
+
+        // GET: BookCopies/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookCopy bookCopy = db.BookCopies.Find(id);
+            if (bookCopy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookCopy);
+        }
+
+        // POST: BookCopies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            BookCopy bookCopy = db.BookCopies.Find(id);
+            db.BookCopies.Remove(bookCopy);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AILibrary/Controllers/LibrariesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AILibrary.Models;
+using AILibrary.Models.Libraries;
+
+namespace AILibrary.Controllers
+{
+    public class LibrariesController : Controller
+    {
+        private LibraryDBContext db = new LibraryDBContext();
+
+        // GET: Libraries
+        public ActionResult Index()
+        {
+            return View(db.Libraries.ToList());
+        }
+
+        // GET: Libraries/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Library library = db.Libraries.Find(id);
+            if (library == null)
+            {
+                return HttpNotFound();
+            }
+            return View(library);
+        }
+
+        // GET: Libraries/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Libraries/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,PossesorId")] Library library)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Libraries.Add(library);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(library);
+        }
+
+        // GET: Libraries/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Library library = db.Libraries.Find(id);
+            if (library == null)
+            {
+                return HttpNotFound();
+            }
+            return View(library);
+        }
+
+        // POST: Libraries/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,PossesorId")] Library library)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(library).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(library);
+        }
+
+        // GET: Libraries/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Library library = db.Libraries.Find(id);
+            if (library == null)
+            {
+                return HttpNotFound();
+            }
+            return View(library);
+        }
+
+        // POST: Libraries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Library library = db.Libraries.Find(id);
+            db.Libraries.Remove(library);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
