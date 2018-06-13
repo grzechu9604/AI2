58220fa34588f2c62a63db42894ccec001ae2f18
Grzegorz Wilczyński <grzegorz.wilczynski@student.put.poznan.pl>
2018-06-13 11:20:40 +0200

    Usuwanie po 24 godzinach

--- AILibrary/Controllers/AccountController.cs
+++ AILibrary/Controllers/AccountController.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using AILibrary.Models;
 using AILibrary.Views.Helpers;
+using AILibrary.Controllers.Helpers;
+using System.Configuration;
 
 namespace AILibrary.Controllers
 {
@@ -166,14 +168,16 @@ namespace AILibrary.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    //await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
 
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                   
+
+                    DelegateUserDeletion(user);
+
                     return await RedirectToInfo(TextElements.MailConfirmationMessage, TextElements.MailConfirmationTitle, TextElements.MailConfirmationHeader);
                 }
                 AddErrors(result);
@@ -499,6 +503,20 @@ namespace AILibrary.Controllers
             Session[MyConstants.VBHeader] = header;
 
             return View("Info");
+        }
+
+        private void DelegateUserDeletion(ApplicationUser userToDelete)
+        {
+            long intervalInHours = Convert.ToInt64(ConfigurationManager.AppSettings["hoursToDeleteUnconfirmedAccount"]);
+
+            var deletionExecutor = new DeleteAccountExecutor()
+            {
+                Manager = UserManager,
+                UserToDelete = userToDelete
+            };
+
+            deletionExecutor.SetIntervalUsingHours(intervalInHours);
+            deletionExecutor.DelegateDelete();
         }
         #endregion
     }
---
+++ AILibrary/Controllers/Helpers/DeleteAccountExecutor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+using System.Threading.Tasks;
+using System.Web;
+using AILibrary.Models;
+using Microsoft.AspNet.Identity;
+
+namespace AILibrary.Controllers.Helpers
+{
+    public class DeleteAccountExecutor
+    {
+        public ApplicationUser UserToDelete { get; set; }
+        public ApplicationUserManager Manager { get; set; }
+        public HttpContextBase context;
+
+        private long? Interval;
+        private Timer Timer;
+
+        public void DelegateDelete()
+        {
+            if (UserToDelete == null)
+            {
+                throw new Exception("User to delete is not chosen!");
+            }
+
+            if (Manager == null)
+            {
+                throw new Exception("ApplicationUserManager is not set!");
+            }
+
+            if (!Interval.HasValue)
+            {
+                throw new Exception("Interval is not set!");
+            }
+
+            Timer = new Timer();
+            Timer.Interval = Interval.Value;
+            Timer.Elapsed += DeleteUser;
+            Timer.AutoReset = false;
+            Timer.Start();
+        }
+
+        public void SetIntervalUsingMiliseconds(long interval)
+        {
+            Interval = interval;
+        }
+
+        public void SetIntervalUsingSeconds(long interval)
+        {
+            Interval = interval * 1000;
+        }
+
+        public void SetIntervalUsingMinutes(long interval)
+        {
+            Interval = interval * 1000 * 60;
+        }
+
+        public void SetIntervalUsingHours(long interval)
+        {
+            Interval = interval * 1000 * 60 * 60;
+        }
+
+        private void DeleteUser(object sender, ElapsedEventArgs e)
+        {
+            Timer.Stop();
+
+            var updatedUserToDelete = new ApplicationDbContext().Users.FirstOrDefault(user => user.Id.Equals(UserToDelete.Id) && !user.EmailConfirmed);
+
+            if (updatedUserToDelete != null)
+            {
+                Manager.Delete(updatedUserToDelete);
+            }
+        }
+    }
+}
